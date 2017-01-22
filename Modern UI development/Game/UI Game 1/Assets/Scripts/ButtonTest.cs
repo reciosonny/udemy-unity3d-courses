4c2c88837be81eb1247307e35546888474347467
Sonny R. Recio <reciosonny@gmail.com>
2017-01-22 15:26:07 +0800

    Unity UI course update 1

---
+++ Modern UI development/Game/UI Game 1/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+
+	public Slider Slider;
+
+	public void DoSomething() {
+		Debug.Log (Slider.value.ToString());
+	}
+
+	public void DoSomethingWithSlider(Slider slider) {
+		Debug.Log (slider.value.ToString());
+	}
+
+}
