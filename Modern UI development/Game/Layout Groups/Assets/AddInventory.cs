9051a951f36c98bb6aa6e47218dd0f9d0655ef84
Sonny Recio <reciosonny@gmail.com>
2017-01-26 23:33:07 +0800

    UI course - main menu pt. 1

---
+++ Modern UI development/Game/Layout Groups/Assets/AddInventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddInventory : MonoBehaviour {
+    public Button btnInventory;
+    public Transform panelInventory;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private int count = 0;
+    public void OnClick() {
+        count += 1;
+        Debug.Log("you just clicked me");
+        var newItem = Instantiate(btnInventory);
+        var itemTxt = btnInventory.GetComponentInChildren<Text>();
+        itemTxt.text = "item " + count;
+
+        newItem.transform.parent = panelInventory;
+    }
+
+
+}
