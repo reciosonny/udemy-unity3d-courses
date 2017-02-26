19bcb1e89935c95cff26c0eff095ead8484cd912
Sonny R. Recio <reciosonny@gmail.com>
2017-02-26 18:33:04 +0800

    Making flappy bird game - pt. 1

---
+++ Making Mobile Games like a pro/Flappy Bird/Assets/Scripts/Bird Script/BirdScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BirdScript : MonoBehaviour {
+
+	public static BirdScript instance;
+	[SerializeField]
+	private Rigidbody2D myRigidBody;
+	[SerializeField]
+	private Animator anim;
+
+	private float forwardSpeed = 3f;
+	private float bounceSpeed = 4f;
+
+	private bool didFlap;
+	public bool isAlive;
+
+	private Button flapButton;
+
+	void Awake() {
+//		myRigidBody = GetComponent<Rigidbody2D> (); // less efficient assigning of physics
+		if (instance == null) {
+			instance = this;
+		}
+		isAlive = true;
+
+		flapButton = GameObject.FindGameObjectWithTag ("FlapButton").GetComponent<Button> ();
+		flapButton.onClick.AddListener (() => {
+			FlapTheBird();
+		});
+
+		SetCamerasX ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate () {
+		if (isAlive) {
+			Vector3 temp = transform.position;
+			temp.x += forwardSpeed * Time.deltaTime;
+			transform.position = temp;
+
+			if (didFlap) {
+				didFlap = false;
+				myRigidBody.velocity = new Vector2 (0, bounceSpeed);
+				anim.SetTrigger ("Flap");
+			}
+
+			if (myRigidBody.velocity.y >= 0) {
+				transform.rotation = Quaternion.Euler (0, 0, 0);
+			} else {
+				float angle = 0;
+				angle = Mathf.Lerp (0,-90,-myRigidBody.velocity.y / 7);
+				transform.rotation = Quaternion.Euler (0,0,angle);
+			}
+		}
+	}
+
+	void SetCamerasX() {
+		CameraScript.offsetX = (Camera.main.transform.position.x - transform.position.x) - 1f;
+	}
+
+	public float GetPositionX() {
+		return transform.position.x;
+	}
+
+	public void FlapTheBird() {
+		didFlap = true;
+	}
+
+
+
+
+
+}
---
+++ Modern UI development/Game/Main Menu System/Assets/BtnManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button[] btns = GetComponentsInChildren <Button>();
+		foreach(Button btn in btns) {
+			btn.onClick.AddListener (delegate() {
+				Debug.Log("clicked!");
+			});
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
